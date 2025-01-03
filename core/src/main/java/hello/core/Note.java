60e061bef84ed7ce4a9282c533c392bb2912586f
Songpenga <iiuuyy6654@gmail.com>
2025-01-03 16:10:51 +0900

    SECTION5. 스프링 빈 조회

--- core/src/main/java/hello/core/Note.java
+++ core/src/main/java/hello/core/Note.java
@@ -5,6 +5,12 @@ import hello.core.discount.RateDiscountPolicy;
 
 public class Note {
     /*
+    * 25.01.03 스프링 빈 조회 - 동일한 타입이 둘 이상
+    * 타입으로 조회시 같은 타입의 스프링 빈이 들 이상이면 오류가 발생한다. 이떄는 빈 이름을 지정해야된다.
+    * ac.getBeansOfType() 을 사용하면 해당 타입의 모든 빈을 조회할 수 있다.
+    */
+    
+    /*
     * 25.01.02
     * AppConging의 등장으로 애플리케이션이 사용영역과 구성영역으로 분리되었다.
     * 사용영역 : OrderServiceImpl, DiscountPolicy, FixDiscountPolicy, RateDiscountPolicy
--- core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -6,7 +6,10 @@ import hello.core.MemberInfo.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
     AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Appconfig.class);
@@ -29,10 +32,24 @@ public class ApplicationContextBasicFindTest {
     }
 
     @Test
-    @DisplayName("구체 타입으로 조회")
-    void findBeanByType(){
-        MemberService memberService = app.getBean(MemberService.class);
+    @DisplayName("구체 타입으로 조회") // => 유연성이 떨어진다.
+    void findBeanByName2(){
+        MemberServiceImpl memberService = app.getBean(MemberServiceImpl.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
 
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByNameX(){
+/*
+        app.getBean("xxxxx", MemberService.class);
+        MemberService xxxxx = app.getBean("xxxxx", MemberService.class);
+*/
+        // => org.springframework.beans.factory.NoSuchBeanDefinitionException: No bean named 'xxxxx' available
+
+        assertThrows(NoSuchBeanDefinitionException.class,
+                ()->app.getBean("xxxxx", MemberService.class));
+
+    }
 }
+
