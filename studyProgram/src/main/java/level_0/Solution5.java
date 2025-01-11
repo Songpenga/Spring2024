c8c0947f714ced356f53c96c5ea6fb2746e357e5
Songpenga <iiuuyy6654@gmail.com>
2025-01-12 00:19:26 +0900

    Create Solution5.java

---
+++ studyProgram/src/main/java/level_0/Solution5.java
@@ -0,0 +1,17 @@
+package level_0;
+
+import java.util.Arrays;
+
+public class Solution5 {
+    
+	
+    public static void main(String[] args) {
+    	System.out.print("!@#$%^&*(\\'\"<>?:;");
+    	//특수문자 '" 을 출력하려면 \을 붙여야 한다.
+    }
+
+    
+}
+
+
+
