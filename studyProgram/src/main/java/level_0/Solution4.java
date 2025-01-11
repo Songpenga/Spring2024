375bb3ec43ee4352a13472d5d955cc9ec6572d0a
Songpenga <iiuuyy6654@gmail.com>
2025-01-11 23:11:53 +0900

    Create Solution4.java

---
+++ studyProgram/src/main/java/level_0/Solution4.java
@@ -0,0 +1,23 @@
+package level_0;
+
+import java.util.Arrays;
+
+public class Solution4 {
+    
+	
+    public static void main(String[] args) {
+        int sc = 1;
+        int test = sc%2;
+        
+        if(test == 1){
+            System.out.println(sc + "is edd");
+        }else{
+            System.out.println(sc + "is even");
+        }
+    }
+
+    
+}
+
+
+
