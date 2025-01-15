f3a2d1905b8213b6ed727b817095df00b4dc8b5e
Songpenga <iiuuyy6654@gmail.com>
2025-01-15 23:49:41 +0900

    Create Solution7.java

---
+++ studyProgram/src/main/java/level_0/Solution7.java
@@ -0,0 +1,26 @@
+package level_0;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Solution7 {
+    
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int sum = a+b;
+	        System.out.printf("a + b = "+ a+b);
+	        
+	        
+	        //[참고]
+	        System.out.printf("%d + %d = %d",a,b,a+b);
+	        System.out.println(a + " + " + b + " = " + (a + b));
+	    }
+
+    
+}
+
+
+
