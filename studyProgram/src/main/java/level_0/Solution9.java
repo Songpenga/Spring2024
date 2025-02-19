0e0a2c067c55c08ff25ceaa288d483875f99a78d
Songpenga <iiuuyy6654@gmail.com>
2025-02-19 23:49:53 +0900

    Create Solution9.java

---
+++ studyProgram/src/main/java/level_0/Solution9.java
@@ -0,0 +1,22 @@
+package level_0;
+
+import java.util.Arrays;
+
+public class Solution9 {
+    
+    public int solution(int num1, int num2) {
+
+        if(num1 == num2) {
+             int answer = 1;
+             return answer;
+        }
+
+        else {
+             int answer = -1;
+             return answer;
+        }
+    }    
+}
+
+
+
