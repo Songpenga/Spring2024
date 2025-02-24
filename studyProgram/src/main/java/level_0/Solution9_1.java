cd3d8a1746ab3989ce0734fc0f38e4a2126a8b2b
Songpenga <iiuuyy6654@gmail.com>
2025-02-24 19:45:40 +0900

    Create Solution9_1.java

---
+++ studyProgram/src/main/java/level_0/Solution9_1.java
@@ -0,0 +1,12 @@
+package level_0;
+
+public class Solution9_1 {
+    
+	   public int solution(int num1, int num2) {
+	        int answer = (num1 == num2) ? 1 : -1;
+	        return answer;
+	    }   
+}
+
+
+
