7765baafd0d2d2a0eabff4687b2c05ad65119ccb
Songpenga <iiuuyy6654@gmail.com>
2025-03-03 00:04:49 +0900

    Create Solution10.java

---
+++ studyProgram/src/main/java/level_0/Solution10.java
@@ -0,0 +1,18 @@
+package level_0;
+
+import java.util.Arrays;
+
+public class Solution10 {
+    
+    public int solution(int age) {
+        int answer = 0;
+        int year = 2022;
+        answer = year - age +1;
+
+        System.out.println(answer);
+        return answer;
+    }  
+}
+
+
+
