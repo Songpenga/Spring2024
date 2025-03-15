4313d2f3940a747c5bb91ca13690852758f82aac
Songpenga <iiuuyy6654@gmail.com>
2025-03-15 15:30:15 +0900

    Create Solution12_1.java

---
+++ studyProgram/src/main/java/level_0/Solution12_1.java
@@ -0,0 +1,18 @@
+package level_0;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution12_1 {    
+	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print(br.readLine().replaceAll(" ", ""));
+    }    
+}
+
+
+
