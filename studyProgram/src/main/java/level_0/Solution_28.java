cc9c6cc4076efbd9f6c453a6ec6b1b6fc3ce7413
Peng <iiuuyy6654@gmail.com>
2025-05-17 23:51:20 +0900

    Create Solution_28.java

---
+++ studyProgram/src/main/java/level_0/Solution_28.java
@@ -0,0 +1,20 @@
+package level_0;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution_28 {
+    public static void main(String[] args) {
+        	int money = 0;
+            int[] answer = new int[2];
+            
+            answer[0] = money/5500;
+            answer[1] = money%5500;
+                
+    }	
+
+}
+
+
+
